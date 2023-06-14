5c7516e1c72261a65d6ed0f03b0739fc82d4f307
Lucas52352 <lucasrius86@gmail.com>
2023-06-14 09:20:49 -0300

    structure

---
+++ wiseWalletClient/src/Screens/Shared.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const SharedScreen = () => {
+    return (
+        <View>
+            <Text>Shared Screen</Text>
+        </View>
+    )
+}
+
+export default SharedScreen
