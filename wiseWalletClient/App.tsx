10764c870faf0768973e5d6a4a6fdcf7c2f9edfe
Lucas52352 <lucasrius86@gmail.com>
2023-06-16 12:14:23 -0300

    aaaaa

--- wiseWalletClient/App.tsx
+++ wiseWalletClient/App.tsx
@@ -12,13 +12,14 @@ import {
 import Navigator from './src/Navigation/HomeStack'
 import { Provider } from 'react-redux';
 import {store} from "./src/redux/store";
+import Navigate from './src/navigate/Navigate';
 
 function App(): JSX.Element {
   return (
 
     <Auth0Provider domain={"walletwise.us.auth0.com"} clientId={"o4nR12XFZsF5SOiAbWxHhi3bBEU0DiKp"}>
   <Provider store={store}>
-    <Navigator/>
+    <Navigate/>
   </Provider>
   </Auth0Provider>
 
--- wiseWalletClient/src/components/Login/Login.tsx
+++ wiseWalletClient/src/components/Login/Login.tsx
@@ -1,53 +1,29 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  ImageBackground,
+  StatusBar,
+  Button,
+  Alert,
+} from 'react-native';
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
 
-import { StyleSheet, Text, View, Image, ScrollView , TouchableOpacity, ImageBackground, StatusBar, Button, Alert} from 'react-native';
-import {useAuth0, Auth0Provider} from 'react-native-auth0';
-
-// const Login = ({navigation}:any)=>{
-//   const { authorize } = useAuth0();
-
-//   const onPress = async () => {
-//     try {
-//       await authorize();
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-//     return(
-      
-      
-        // 
-        // <View style={styles.container}>
-        //
-        // <View>
-    //     
-    //     <TouchableOpacity style={styles.button}>
-    //         <Text style={styles.butonText}>Register</Text>
-    //     </TouchableOpacity>
-    //     </View> 
-    //       
-    //         </View>
-    //     </ImageBackground>
-    //   </View>
-  
-//     <Auth0Provider domain="walletwise.us.auth0.com" clientId="o4nR12XFZsF5SOiAbWxHhi3bBEU0DiKp">
-//     <View style={styles.container}>
-//       <Button onPress={onPress} title="Log in" />
-//     </View>
-//   </Auth0Provider>
-//     )
-// }
 const LoginButton = () => {
-  const {authorize, user} = useAuth0();
-  if(user){
-    console.log(user)
+  const { authorize, user } = useAuth0();
+  if (user) {
+    console.log(user);
   }
-  const {clearSession} = useAuth0();
+  const { clearSession } = useAuth0();
   const onPress = async () => {
-    if(authorize) {
+    if (authorize) {
       try {
-        console.log(authorize)
+        console.log(authorize);
         console.log(user);
-        
+
         await authorize();
       } catch (error) {
         console.log(error);
@@ -56,90 +32,90 @@ const LoginButton = () => {
   };
   const logOut = async () => {
     try {
-        await clearSession();
+      await clearSession();
     } catch (e) {
-        console.log(e);
+      console.log(e);
     }
-};
+  };
 
-  return(
+  return (
     <View style={styles.scrollContainer}>
-    <ImageBackground source={require('../img/fondo2.png')} style={styles.foto}>
-    <StatusBar
-      animated={true}
-      backgroundColor="transparent"
-      barStyle={"dark-content"}
-      translucent={true}
-      />
-      <View style={styles.container}>
-      <Image style={styles.img} source={require('../img/logo.png')} />
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-           <Text style={styles.butonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={logOut}>
-           <Text style={styles.butonText}>Register</Text>
-      </TouchableOpacity>
-      <Text style={styles.h12}>Forgot password?</Text>
-      </View>
+      <ImageBackground
+        source={require('../img/fondo2.png')}
+        style={styles.foto}>
+        <StatusBar
+          animated={true}
+          backgroundColor="transparent"
+          barStyle={'dark-content'}
+          translucent={true}
+        />
+        <View style={styles.container}>
+          <Image style={styles.img} source={require('../img/logo.png')} />
+          <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.butonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={logOut}>
+            <Text style={styles.butonText}>Register</Text>
+          </TouchableOpacity>
+          <Text style={styles.h12}>Forgot password?</Text>
+        </View>
       </ImageBackground>
     </View>
-  ) 
-  
-}
+  );
+};
 
 export default LoginButton;
 
 const styles = StyleSheet.create({
-    foto:{
-        flex: 1,
-        padding:20
-    },
-    butonText: {
-        fontSize: 18,
-        color: "white",
-        fontWeight: "800",
-    },
-    scrollContainer : {
-        flex:1
-    },
-    container: {
-      display: 'flex',
-      justifyContent: "center",
-      alignItems: "center",
-      height: 600,
-    },
-    button: {
-      margin: 15,
-      backgroundColor: "#c29a2b",
-      color: "white",
-      width: 280,
-      height: 50,
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 0,
-      borderRadius: 6,
-    },
-    h1:{
-      color: 'white',
-    },
-    h12:{
-      color: 'white',
-      paddingBottom: 30
-    },
-    img: {
-      width: 200,
-      height: 220
-    },
-    input: {
-      height: 40,
-      width: 240,
-      margin: 5,
-      borderWidth: 1,
-      padding: 20,
-      backgroundColor: '#fafafa0',
-      border: "1px solid white",
-      borderRadius: 5,
-      color: "#cccccc"
-    }
-  });
-
+  foto: {
+    flex: 1,
+    padding: 20,
+  },
+  butonText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: '800',
+  },
+  scrollContainer: {
+    flex: 1,
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 600,
+  },
+  button: {
+    margin: 15,
+    backgroundColor: '#c29a2b',
+    color: 'white',
+    width: 280,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 0,
+    borderRadius: 6,
+  },
+  h1: {
+    color: 'white',
+  },
+  h12: {
+    color: 'white',
+    paddingBottom: 30,
+  },
+  img: {
+    width: 200,
+    height: 220,
+  },
+  input: {
+    height: 40,
+    width: 240,
+    margin: 5,
+    borderWidth: 1,
+    padding: 20,
+    backgroundColor: '#fafafa0',
+    border: '1px solid white',
+    borderRadius: 5,
+    color: '#cccccc',
+  },
+});
