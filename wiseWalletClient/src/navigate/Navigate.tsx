2dc593b37196b8558969bb3aa95adab6be707bf5
Matias Hernandez <matias2k00@gmail.com>
2023-07-01 20:34:12 -0300

    filtros, ordenamiento y avance del component premium

--- wiseWalletClient/src/navigate/Navigate.tsx
+++ wiseWalletClient/src/navigate/Navigate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer, RouteProp} from '@react-navigation/native';
+import { NavigationContainer, RouteProp } from '@react-navigation/native';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import Slider from '../slider/Slider';
 import MyTabs from '../tabs/Tabs';
@@ -7,15 +7,13 @@ import AuthView from '../Screens/Authenticate';
 import Intro from '../components/Intro/Intro';
 import MyDrawer from '../components/drawer';
 import DetailIncome from '../components/home/DetailIncome';
-import DetailExpense from '../components/home/DetailExpense'
+import DetailExpense from '../components/home/DetailExpense';
 import CloudinaryComponent from '../components/goals/Goals';
 import GoalList from '../components/goals/GoalsList';
 import GoalDetail from '../components/goals/GoalDetail';
 import SharedList from '../components/shared/SharedList';
 import NewShared from '../components/shared/NewShared';
-
-
-
+import Premium from '../components/Premium/Premium';
 type RootStackParamList = {
   Intro: undefined;
   Login: undefined;
@@ -26,23 +24,20 @@ type RootStackParamList = {
   Goals: undefined;
   GoalsList: undefined;
   GoalDetail: undefined;
-
   SharedList: undefined;
   NewShared: undefined;
-
+  Premium: undefined;
 };
 
 type SliderScreenRouteProp = RouteProp<RootStackParamList, 'Slider'>;
 type DetailScreenRouteProp = RouteProp<RootStackParamList, 'DetailIncome'>;
 type DetailExpenseRouteProp = RouteProp<RootStackParamList, 'DetailExpense'>;
 
-
 export type Props = {
   route: SliderScreenRouteProp;
   routeDetail: DetailScreenRouteProp;
   routeExpense: DetailExpenseRouteProp;
-};;
-
+};
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -50,10 +45,10 @@ const Navigate = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-
         <Stack.Screen name="Intro" component={Intro} />
-        <Stack.Screen name="Login" component={AuthView} /> 
-        <Stack.Screen name="Slider" component={Slider} />  
+        <Stack.Screen name="Login" component={AuthView} />
+        <Stack.Screen name="Premium" component={Premium} />
+        <Stack.Screen name="Slider" component={Slider} />
         <Stack.Screen name="MyDrawer" component={MyDrawer} />
         <Stack.Screen name="DetailIncome" component={DetailIncome} />
         <Stack.Screen name="DetailExpense" component={DetailExpense} />
@@ -66,6 +61,5 @@ const Navigate = () => {
     </NavigationContainer>
   );
 };
-
 
 export default Navigate;
