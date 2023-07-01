c792059bf2f13539b884bc0601b1952b77dd388d
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-07-01 17:24:22 -0300

    logica y componente del shareddetail

--- wiseWalletClient/src/redux/slices/sharedSlice.ts
+++ wiseWalletClient/src/redux/slices/sharedSlice.ts
@@ -14,8 +14,10 @@ export interface participants {
 export interface detailRoom {
     id:number
     participants: participants[]
-    personalExpense: IExpenses
+    personalExpense: number
     total:number
+    name: string
+    deletedShared: boolean
 }
 
 export interface sharedState {
@@ -23,29 +25,61 @@ export interface sharedState {
     loading: boolean,
     error: string | null, 
     roomId: number,
+    detail: detailRoom
 }
 
 const initialState : sharedState = {
     allRooms : [],
     loading:false,
     error: null,
-    roomId: 0,
+    roomId: 0, 
+    detail:{
+        id:0,
+        participants:[],
+        personalExpense:0,
+        total:0,
+        name:'',
+        deletedShared: false
+    }
 }
 
 const sharedSlice = createSlice ({
     name:"shared",
     initialState,
     reducers:{
-        setShared: (state, action: PayloadAction<detailRoom>) =>{
-            //state.allRooms= action.payload,
+        setShared: (state, action: PayloadAction<detailRoom[]>) =>{
+            state.allRooms= action.payload
         },
+        setDetail:(state, action: PayloadAction<detailRoom>)=> {
+            state.detail= action.payload
+        },
+        setRoomId:(state, action: PayloadAction<number>)=>{
+            state.roomId= action.payload
+        },
+        
         
     },
 
     
 })
 
-export const {setShared} = sharedSlice.actions;
+export const {setShared, setDetail, setRoomId} = sharedSlice.actions;
+
+export const getDetail=(id: number): AppThunk=>{
+    return async (dispatch)=>{
+        const response= await axios.get(`${base_URL}/shared/${id}`);
+        console.log(response.data, "este es el response")
+        dispatch(setDetail( response.data));
+    }
+}
+
+export const getAllRooms=(id: number): AppThunk=>{
+    return async (dispatch)=>{
+        const response= await axios.get(`${base_URL}/all/shared/${id}`)
+        dispatch(setShared(response.data))
+    }
+}
+
 export default sharedSlice.reducer;
 
 
