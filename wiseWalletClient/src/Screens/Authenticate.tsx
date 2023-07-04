5a46e9baf3c018eb4742a08e4fe85517337d18e8
Matias Hernandez <matias2k00@gmail.com>
2023-07-04 20:07:06 -0300

    primeros cambios de async en intro

--- wiseWalletClient/src/Screens/Authenticate.tsx
+++ wiseWalletClient/src/Screens/Authenticate.tsx
@@ -1,37 +1,64 @@
 import React, { useState } from 'react';
-import { View, StyleSheet , TouchableOpacity, Text, Image, ScrollView} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+  ScrollView,
+} from 'react-native';
 import Login from '../components/Login/Login';
 import Register from '../components/Login/Register';
+import { useNavigation } from '@react-navigation/native';
 
 const AuthView: React.FC = () => {
   const [isLoginView, setIsLoginView] = useState(false);
+  const navigation: any = useNavigation();
   const handleToggleView = () => {
     setIsLoginView(!isLoginView);
   };
 
   return (
-    <View style={{backgroundColor:"black"}}>
-        {isLoginView ? <Register /> :  <Login />}
-        <TouchableOpacity onPress={handleToggleView} style={styles.botonRegister}>
-          {isLoginView ? <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, textAlign: 'center'}}>Sign In</Text> : <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, textAlign: 'center'}}>Sign Up</Text>}
-        </TouchableOpacity>
-
+    <View style={{ backgroundColor: 'black' }}>
+      {isLoginView ? <Register /> : <Login />}
+      <TouchableOpacity onPress={handleToggleView} style={styles.botonRegister}>
+        {isLoginView ? (
+          <Text
+            style={{
+              color: 'white',
+              fontWeight: 'bold',
+              fontSize: 20,
+              textAlign: 'center',
+            }}>
+            Sign In
+          </Text>
+        ) : (
+          <Text
+            style={{
+              color: 'white',
+              fontWeight: 'bold',
+              fontSize: 20,
+              textAlign: 'center',
+            }}>
+            Sign Up
+          </Text>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-  botonRegister:{
+  botonRegister: {
     width: 150,
     position: 'absolute',
     alignSelf: 'center',
-    backgroundColor: "#4285F4",
-    borderRadius:10,
-    height: 50 ,
-    justifyContent: "center",
-    top: "90%"
-  }
-})
+    backgroundColor: '#4285F4',
+    borderRadius: 10,
+    height: 50,
+    justifyContent: 'center',
+    top: '90%',
+  },
+});
 
 export default AuthView;
